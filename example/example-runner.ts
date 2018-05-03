e01e13b9e60111b2272e46fb7f63ea9eebe0292d
Ze Ye <zeye@microsoft.com>
2018-05-03 11:26:30 +0800

    update example util

--- example/example-runner.ts
+++ example/example-runner.ts
@@ -7,6 +7,6 @@ let logger = new Logger('[Example]', LogSeverity.DEBUG);
 export function runExample(srcData: any, mappingConfig: StringStyledMappingConfig): void {
   logger.debug('Start execute example...');
   let result = convert(srcData, mappingConfig);
-  logger.debug(result);
+  logger.debug(JSON.stringify(result, null, '\t'));
   logger.debug('Finish execution.')
 }
